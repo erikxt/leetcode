e8f558e68fb6b5ab566d4509040ef26b555edea3
erikxt <417798>
2015-07-20 12:37:44 +0800

    format code

--- Sliding Window Maximum.py
+++ Sliding Window Maximum.py
@@ -12,16 +12,13 @@ class Solution:
         for i in range(0, k):
             while tmp != [] and nums[i] > nums[tmp[-1]]:
                 tmp.pop()
-                n += 1
             tmp.append(i)
         for i in range(k, len(nums)):
             ans.append(nums[tmp[0]])
             while tmp != [] and nums[i] > nums[tmp[-1]]:
-                m += 1
                 tmp.pop()
             tmp.append(i)
             while tmp != [] and tmp[0] <= i - k:
-                m += 1
                 tmp.pop(0)
         ans.append(nums[tmp[0]])
         return ans

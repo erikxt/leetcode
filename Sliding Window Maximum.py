7e80e797455af1e33caf3e4ae7378a2e9cd0086b
erikxt <417798>
2015-07-20 12:34:11 +0800

    pay attention to deque

---
+++ Sliding Window Maximum.py
@@ -0,0 +1,37 @@
+__author__ = 'erik'
+
+
+class Solution:
+    # @param {integer[]} nums
+    # @param {integer} k
+    # @return {integer[]}
+    def maxSlidingWindow(self, nums, k):
+        if len(nums) == 0:
+            return []
+        ans, tmp = [], []
+        for i in range(0, k):
+            n = 0
+            while tmp != [] and nums[i] > nums[tmp[-1]]:
+                tmp.pop()
+                n += 1
+            tmp.append(i)
+        print n
+        for i in range(k, len(nums)):
+            ans.append(nums[tmp[0]])
+            m = 0
+            while tmp != [] and nums[i] > nums[tmp[-1]]:
+                m += 1
+                tmp.pop()
+            tmp.append(i)
+            while tmp != [] and tmp[0] <= i - k:
+                m += 1
+                tmp.pop(0)
+            print('m=%s') % m
+        ans.append(nums[tmp[0]])
+        return ans
+
+if __name__ == '__main__':
+    nums = (1, 3, 1, 2, 0, 5)
+    k = 3
+    s = Solution()
+    print s.maxSlidingWindow(nums, k)

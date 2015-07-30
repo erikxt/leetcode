368e46c991398521a96600a2b8b2c80ad621ef36
erik <iqboy118@gmail.com>
2015-07-31 01:13:29 +0800

    @wuhan

---
+++ Sort List.py
@@ -0,0 +1,17 @@
+__author__ = 'erica'
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param {ListNode} head
+    # @return {ListNode}
+    def sortList(self, head):
+        pass
+
+    def mergeSort(self, head1, head2):
+        pass
